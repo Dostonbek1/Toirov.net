82d51ab47f88620aa591bc822cb982190d6442b1
Dostonbek Toirov <31990136+Dostonbek1@users.noreply.github.com>
2018-01-12 23:47:19 -0500

    Add files via upload

---
+++ content_management.py
@@ -0,0 +1,9 @@
+
+def Content():
+
+    TOPIC_DICT = {"Basics":[["1. Why Python 3? Python 2 vs Python 3", "https://www.youtube.com/watch?v=oVp1vrfL_w4&list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M"],
+                            ["2. Installing Python 3 - How to install/use both Python 2 and Python 3", "https://www.youtube.com/watch?v=IX6mc9l6tY4&index=2&list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M"],
+                            ["3. Print Function and Strings", "https://www.youtube.com/watch?v=UsCQXe1OHZk&index=3&list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M"]
+                            ]}
+
+    return TOPIC_DICT

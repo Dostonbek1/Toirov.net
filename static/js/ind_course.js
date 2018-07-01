40f35f7b47dd73b91007e3a01964e6f61246f6dc
Dostonbek <toirovd@berea.edu>
2018-07-01 14:17:00 -0400

    Created ind_course template for other courses

--- static/js/ind_course.js
+++ static/js/ind_course.js
@@ -35,18 +35,3 @@ window.onload=function(){
     }   
 
 };
-
-var links = document.getElementsByClassName("link") ;
-    console.log(links)
-    for (var i=0; i<links.length; i++) {
-        links[i].onclick = handler;
-        console.log(links[i]);
-    }
-
-    function handler(e) {
-        videotarget = this.getAttribute("id");
-        var video_player = document.getElementById('video-iframe');
-        if (videotarget != video_player.src) {
-            video_player.src = videotarget;
-        }
-    }
